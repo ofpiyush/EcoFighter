de9f11b1478d18134e4b746212b7ed1d877cc56c
Piyush Mishra <piyush@codeitout.com>
2018-11-11 22:58:14 +0530

    Added fuel sign, good old road fighter days

--- HybridBot/Assets/Scripts/BallController.cs
+++ HybridBot/Assets/Scripts/BallController.cs
@@ -6,9 +6,15 @@ public class BallController : MonoBehaviour
 {
 
     public float speedMultiplier = 5f;
-    public float topSpeed = 250f;
+    public float fuelChargeRate = 7f;
+    public float fuelBurnRate = 5f;
+    public float topSpeed = 15f;
+
+    public float maxFuel = 100f;
     float forwardThrust = 0f;
     float sideThrust = 0f;
+    public float fuel = 0f;
+    public float currentSpeed = 0f;
 
     Rigidbody rb;
     public Transform racer;
@@ -22,13 +28,14 @@ public class BallController : MonoBehaviour
         audioData = GetComponent<AudioSource>();
         rb = GetComponent<Rigidbody>();
         audioData.loop = true;
+        fuel = maxFuel / 2f;
     }
     void Update()
     {
         forwardThrust = Input.GetAxis("Vertical");
         sideThrust = Input.GetAxis("Horizontal");
 
-        if (rb.velocity.magnitude > 1f)
+        if (rb.velocity.magnitude > 3f)
         {
             PlayRollingSound();
         }
@@ -36,16 +43,33 @@ public class BallController : MonoBehaviour
         {
             StopAudio();
         }
+        if (GameManager.instance.SunMultiplier > 0f)
+        {
+            fuel += GameManager.instance.SunMultiplier * fuelChargeRate * Time.deltaTime;
+            if (fuel > maxFuel)
+            {
+                fuel = maxFuel;
+            }
+        }
     }
     void FixedUpdate()
     {
 
-        if (GameManager.instance.SunMultiplier > 0f && rb.velocity.magnitude < topSpeed)
+        currentSpeed = rb.velocity.magnitude;
+        if (fuel > 0f && currentSpeed < topSpeed)
         {
-            Vector3 fwd = GameManager.instance.SunMultiplier * (racer.forward * forwardThrust + racer.right * sideThrust * 2f) * Time.deltaTime * speedMultiplier;
+            Vector3 fwd = (racer.forward * forwardThrust + racer.right * sideThrust * 2f) * Time.deltaTime * speedMultiplier;
             rb.AddForce(new Vector3(fwd.x, 0, fwd.z), ForceMode.VelocityChange);
+            if (fwd.magnitude > 0f)
+            {
+                fuel -= fuelBurnRate * Time.deltaTime;
+                if (fuel <= 0f)
+                {
+                    print("ran out of fuel");
+                    fuel = 0f;
+                }
+            }
         }
-
     }
 
     void PlayRollingSound()
--- README.md
+++ README.md
@@ -4,4 +4,19 @@ Theme for GithubGame off this year is *Hybrid*
 
 ## Core theme idea
 
-- Fuel + Solar hybrid bot
+- Low poly + pixel art
+- Fuel + Solar hybrid racer
+- Hybrid of games I loved playing growing up
+
+## Possible directions
+
+- Racing + shooting
+- Racing + platformer
+
+## Possible Game mechanics
+
+- Start as a lame icosphere bot
+- Collect fuel to survive
+- jump and collect items to repair/add power ups?
+- get solar charging capability as a power up
+- Race to leave the planet?
