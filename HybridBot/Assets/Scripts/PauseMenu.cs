b1884ecb1341224fa0c4913b3cd2b57535ea1887
Piyush Mishra <piyush@codeitout.com>
2018-11-29 18:24:34 +0530

    particle system and other changes

--- HybridBot/Assets/Scripts/PauseMenu.cs
+++ HybridBot/Assets/Scripts/PauseMenu.cs
@@ -29,7 +29,7 @@ public class PauseMenu : Gameplay {
 	
 	// Update is called once per frame
 	void Update () {
-		if(Input.GetKeyDown(KeyCode.Escape)) {
+		if(Input.GetKeyDown(KeyCode.Escape) || Input.GetKeyDown("joystick button 7")) {
 			if(IsPaused) {
 				Resume();
 			} else {
